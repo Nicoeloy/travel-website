71df27fdaa3e76c8f81bc09c34930e3f64a07e52
Nicolas Eloy <colasrsca@gmail.com>
2022-02-25 15:38:25 +0100

    fixing img & videos

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,16 @@ import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
 
+import AdventureJungle from '../images/img-9.jpg'
+import Luxury from '../images/img-2.jpg'
+import Mystery from '../images/img-3.jpg'
+import AdventureFootball from '../images/img-4.jpg'
+import Adrenaline from '../images/img-8.jpg'
+
+
+
+
+
 function Cards() {
   return (
     <div className='cards'>
@@ -10,13 +20,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-            src='images/img-9.jpg'
+            src={AdventureJungle}
             text='Explore the hidden waterfall deep inside the Amazon Jungle'
             label='Adventure'
             path='/services'
             />
             <CardItem
-            src='images/img-2.jpg'
+            src={Luxury}
             text='Travel through the Islands of Bali in a Private Cruise'
             label='Luxury'
             path='/services'
@@ -24,22 +34,22 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
+            src={Mystery}
+            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+            label='Mystery'
+            path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
+            src={AdventureFootball}
+            text='Experience Football on Top of the Himilayan Mountains'
+            label='Adventure'
+            path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
+            src={Adrenaline}
+            text='Ride through the Sahara Desert on a guided camel tour'
+            label='Adrenaline'
+            path='/sign-up'
             />
           </ul>
         </div>
